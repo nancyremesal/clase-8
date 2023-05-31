5f9e70a026f8b2bf46f2facaf7a5cbd3c5407c71
nancyremesal <135168648+nancyremesal@users.noreply.github.com>
2023-05-31 17:28:19 -0300

    repo create
    repositorio creado con archivos clase 8

---
+++ script de luciana.js
@@ -0,0 +1,33 @@
+//boton click
+function showAlert(){
+    alert('¡Hola! haz hecho clic en el botón');
+}
+
+//oyente del evento submit
+const form = document.getElementById('myForm');
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+    validateForm();
+});
+
+//verifique los criterios deseados
+function validateEmail(email) {
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,7}$/
+    return regex.test(email)
+}
+function validateForm() {
+    const emailInput = document.getElementById('email');
+    const email = emailInput.value;
+    if (!validateEmail(email)) {
+        alert('Por favor ingrese un correo electrónico válido.');
+    } else {
+        alert('Correo electrónico enviado correctamente.');
+    }
+}
+
+
+document.querySelector("button.button-menu-toggle").addEventListener("click", function() {
+    document.querySelector(".nav-links").classList.toggle("nav-links-responsive")
+});
+
+
